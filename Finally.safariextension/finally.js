3481139970e1838152c1b57fa42b8daec20ccc54
Simon Whitaker <simon@goosoftware.co.uk>
2011-10-19 17:07:10 +0100

    Only show on product pages. Not, for example, the Steve Jobs page of condolence. *cringe*

--- Finally.safariextension/finally.js
+++ Finally.safariextension/finally.js
@@ -1,46 +1,48 @@
-var tags = ['h1', 'h2', 'h3'];
-var headings = [];
+if (document.getElementById('productheader').length > 0 || document.getElementById('productbrowser').length > 0) {
+    var tags = ['h1', 'h2', 'h3'];
+    var headings = [];
 
-// Step down through h1, then h2, then h3 - use the first level of headings we
-// find with text content.
-for (var i = 0; i < tags.length; i++)
-{
-    var temp_headings = document.getElementById('main').getElementsByTagName(tags[i]);
-    for (var j = 0; j < temp_headings.length; j++)
+    // Step down through h1, then h2, then h3 - use the first level of headings we
+    // find with text content.
+    for (var i = 0; i < tags.length; i++)
     {
-        var temp_heading = temp_headings[j];
-        if (temp_headings[j].innerText != '') 
+        var temp_headings = document.getElementById('main').getElementsByTagName(tags[i]);
+        for (var j = 0; j < temp_headings.length; j++)
         {
-            headings.push(temp_headings[j]);
+            var temp_heading = temp_headings[j];
+            if (temp_headings[j].innerText != '') 
+            {
+                headings.push(temp_headings[j]);
+            }
+        }
+        if (headings.length > 0) {
+            break;
         }
     }
-    if (headings.length > 0) {
-        break;
-    }
-}
 
-var epithets = [
-    'Finally.',
-    'At last.',
-    'Better late than never.',
-    'What took us so long?',
-    'It\'s about time.',
-    'You\'ve waited long enough.',
-    'No, seriously.',
-];
+    var epithets = [
+        'Finally.',
+        'At last.',
+        'Better late than never.',
+        'What took us so long?',
+        'It\'s about time.',
+        'You\'ve waited long enough.',
+        'No, seriously.',
+    ];
 
-var epithet_index = 0;
+    var epithet_index = 0;
 
-for (var i = 0; i < headings.length; i++)
-{
-    var h = headings[i];
+    for (var i = 0; i < headings.length; i++)
+    {
+        var h = headings[i];
     
-    if (h.innerText === '' || h.innerText.match("\\?$"))
-        continue;
-    if (!h.innerText.match("\\.$")) {
-        h.innerText = h.innerText + '.';
-    }
-    h.innerText = h.innerText + ' ' + epithets[epithet_index];
+        if (h.innerText === '' || h.innerText.match("\\?$"))
+            continue;
+        if (!h.innerText.match("\\.$")) {
+            h.innerText = h.innerText + '.';
+        }
+        h.innerText = h.innerText + ' ' + epithets[epithet_index];
     
-    epithet_index = (epithet_index + 1) % epithets.length;
+        epithet_index = (epithet_index + 1) % epithets.length;
+    }
 }
